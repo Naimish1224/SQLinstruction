7544710c6eb22f50d62a4c8a4875a0abb1bf251b
naimish1224 <naimish1224@gmail.com>
2021-05-28 15:14:05 -0400

    fixed error

--- PRS/PRSdatabase.sql
+++ PRS/PRSdatabase.sql
@@ -44,7 +44,7 @@ foreign key (UserID) references User(id));
 
 Create table Product (
 ID 						integer 			primary key auto_increment,
-VendorID 				integer 			not null unique,
+VendorID 				integer 			not null,
 PartNumber				varchar(50)			not null,
 ProductName 			varchar(150) 		not null,
 Price					Decimal(10,2) 		not null,
@@ -63,6 +63,43 @@ Quantity 				integer  					,
 CONSTRAINT req_pdt unique (RequestID, ProductID),
 foreign key (ProductId) references Product(Id),
 foreign key (RequestId) references Request(Id));
+
+Select * from user;
+insert into User Values
+(1, 'NP101', 11111, 'Nick', 'Patel', '5134058070', 'NP@Gmail.com', 0, 0),
+(2, 'MP101', 22222, 'Mansi', 'Patel', '5134489588', 'MP@Gmail.com', 0, 0);
+
+select * from vendor;
+insert into Vendor Values
+(1, 'BB-1001', 'Best Buy', '100 Best Buy Street', 'Louisville', 'KY', '40207', '502-111-9099', 'geeksquad@bestbuy.com'),
+(2, 'AP-1001', 'Apple Inc', '1 Infinite Loop', 'Cupertino', 'CA', '95014', '800-123-4567', 'genius@apple.com'),
+(3, 'AM-1001', 'Amazon', '410 Terry Ave. North', 'Seattle', 'WA', '98109','206-266-1000', 'amazon@amazon.com'),
+(4, 'ST-1001', 'Staples', '9550 Mason Montgomery Rd', 'Mason', 'OH', '45040', '513-754-0235', 'support@orders.staples.com'),
+(5, 'MC-1001', 'Micro Center', '11755 Mosteller Rd', 'Sharonville', 'OH', '45241', '513-782-8500', 'support@microcenter.com');
+
+select * from Product;
+insert into Product Values
+(1, 1, 'ME280LL', 'Ipad Mini 2', 296.99, Null, Null),
+(3, 3, '105810', 'Hammermill Paper Premium Multi-Purpose Paper Poly Wrap', 8.99, '1 Ream / 500 Sheets', Null),
+(4, 4, '122374', 'HammerMill® Copy Plus Copy Paper 8 1/2\" x 11\" Case', 29.99, '1 Case, 10 Reams, 500 Sheets per ream', NULL),
+(5, 4, '784551', 'Logitech M325 Wireless Optical Mouse, Ambidextrous Black', 14.99, NULL, NULL),
+(6, 4, '382955', 'Staples Mouse Pad, Black', 2.99, NULL, NULL),
+(7, 4, '2122178','AOC 24-Inch Class LED Monitor', 99.99, NULL, NULL),
+(8, 4, '2460649', 'Laptop HP Notebook 15-AY163NR', 529.99, NULL, NULL),
+(9, 4, '2256788', 'Laptop Dell i3552-3240BLK 15.6\"', 239.99, NULL, NULL),
+(10, 4, 'IM12M9520', 'Laptop Acer Acer™ Aspire One Cloudbook 14\"', 224.99, NULL, NULL),
+(11, 4, '940600', 'Canon imageCLASS Copier (D530)', 99.99, NULL, NULL),
+(12, 5, '228148', 'Acer Aspire ATC-780A-UR12 Desktop Computer', 399.99, NULL, NULL),
+(13, 5, '279364', 'Lenovo IdeaCentre All-In-One Desktop',349.99, NULL, NULL);
+
+
+
+
+
+    
+    DROP USER IF EXISTS prs_user@localhost;
+    Create User prs_user@localhost identified by 'sesame';
+    grant select, insert, delete, update on Prsdb.* to prs_user@localhost;
 
 
 
