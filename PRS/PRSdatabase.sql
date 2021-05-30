0e2aa659733cfb71d481ee36e27e3edd89f85ae6
Naimish1224 <84742161+Naimish1224@users.noreply.github.com>
2021-05-30 08:08:26 -0400

    Update PRSdatabase.sql

--- PRS/PRSdatabase.sql
+++ PRS/PRSdatabase.sql
@@ -4,6 +4,12 @@ CREATE DATABASE PRSdb;
 USE PRSdb;
 
 
+CREATE TABLE Login (
+Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+username                VARCHAR(50) NOT NULL UNIQUE,
+password                VARCHAR(255) NOT NULL,
+created_at              DATETIME DEFAULT CURRENT_TIMESTAMP);
+
 Create table User (
 ID 						integer 			primary key auto_increment,
 Username 				varchar(20) 		not null unique,
